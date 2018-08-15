46915ac369850197e772582833553a61ab4858ad
Leonard Shtika <leoshtika@gmail.com>
2018-08-15 11:11:19 +0300

    Add HomeComponent, ProjectsComponent and config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

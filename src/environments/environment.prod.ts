f5ffd7ed43d55eb96f6b984a1d896f69e4b1556b
Leonard Shtika <leoshtika@gmail.com>
2018-08-19 12:37:36 +0300

    Initialize Capacitor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAqDBHuB_vuXaYh5ZZlnRRR9y3DwPQwKTU',
+    authDomain: 'open-lmp.firebaseapp.com',
+    databaseURL: 'https://open-lmp.firebaseio.com',
+    projectId: 'open-lmp',
+    storageBucket: 'open-lmp.appspot.com',
+    messagingSenderId: '794454019174'
+  }
 };
